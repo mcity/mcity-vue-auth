c14c79a3dfe980c7bfa85bbe8a46b4c8346f524a
Darian Hogue <dhogue@m-c9gf7m32tp.local>
2023-04-21 16:30:17 -0400

    updated store to pinia and updated RefreshAuth to vue3 composition api

--- src/components/RefreshAuthiFrame.vue
+++ src/components/RefreshAuthiFrame.vue
@@ -9,28 +9,25 @@
 </template>
 
 <script>
-import { mapMutations, mapGetters } from 'vuex'
+import { defineComponent, onMounted, ref } from 'vue'
 import queryString from 'query-string'
-export default {
-  data () {
-    return {
-      iframeRef: null,
-      url: null
-    }
-  },
-  computed: {
-    ...mapGetters([
-      'getIframeRef',
-      'getSilentRedirect',
-      'getRefreshAuthURL'
-    ])
-  },
-  methods: {
-    ...mapMutations([
-      'setAccessToken',
-      'setShowIframe'
-    ]),
-    pollIframe (redirectUri, { contentWindow: iframe }) {
+import { computed } from '@vue/reactivity'
+import { useAuthStore } from '../store/session'
+
+export default defineComponent({
+  name: 'RefreshAuthiFrame',
+  setup() {
+    const authStore = useAuthStore()
+    const iframeRef = ref(authStore.iframeRef)
+    const url = ref(null)
+    
+    const getSilentRedirect = ref(authStore.silentRedirectURI)
+    const getRefreshAuthURL = computed(authStore.getRefreshAuthURL)
+
+    const setAccessToken = ref(authStore.setAccessToken)
+    const setShowIframe = ref(authStore.setShowIframe)
+
+    const pollIframe = (redirectUri, { contentWindow: iframe }) => {
       return new Promise((resolve, reject) => {
         const redirectUriPath = queryString.parseUrl(redirectUri).url
 
@@ -59,25 +56,94 @@ export default {
         }, 250)
       })
     }
-  },
-  mounted () {
-    this.iframeRef = document.getElementById('auth-iframe')
-    this.url = this.getRefreshAuthURL
-    this.pollIframe(this.getSilentRedirect, this.iframeRef)
-      .then(({access_token, expires_in}) => {
-        this.setAccessToken(access_token)
-        this.setShowIframe(false)
 
-        setTimeout(() => {
-          this.setShowIframe(true)
-          // Sets timeout value to 10 mins before expires_in
-        }, (expires_in - 10 * 60) * 1000)
-      })
-      .catch(err => { 
-        console.log(err)
-      })
+    onMounted(() => {
+      iframeRef = document.getElementById('auth-iframe')
+      url = getRefreshAuthURL
+      pollIframe(getSilentRedirect, iframeRef)
+        .then(({access_token, expires_in}) => {
+          setAccessToken(access_token)
+          setShowIframe(false)
+
+          setTimeout(() => {
+            setShowIframe(true)
+            // Sets timeout value to 10 mins before expires_in
+          }, (expires_in - 10 * 60) * 1000)
+        })
+        .catch(err => { 
+          console.log(err)
+        })
+    })
   }
-}
+})
+
+// const oldVueSetup = {
+//   data () {
+//     return {
+//       iframeRef: null,
+//       url: null
+//     }
+//   },
+//   computed: {
+//     ...mapGetters([
+//       'getIframeRef',
+//       'getSilentRedirect',
+//       'getRefreshAuthURL'
+//     ])
+//   },
+//   methods: {
+//     ...mapMutations([
+//       'setAccessToken',
+//       'setShowIframe'
+//     ]),
+//     pollIframe (redirectUri, { contentWindow: iframe }) {
+//       return new Promise((resolve, reject) => {
+//         const redirectUriPath = queryString.parseUrl(redirectUri).url
+
+//         let pollingInterval = setInterval(() => {
+//           const iframePath = queryString.parseUrl(iframe.location.href).url
+//           try {
+//             if (iframePath === redirectUriPath) {
+//               if (iframe.location.hash) {
+//                 // Extracts url params from hash segment
+//                 const params = queryString.parse(iframe.location.hash)
+
+//                 if (params.error) {
+//                   reject(new Error(params.error))
+//                 } else {
+//                   resolve(params)
+//                 }
+//               } else {
+//                 reject(new Error('OAuth redirect has occurred but no query or hash parameters were found.'))
+//               }
+//               clearInterval(pollingInterval)
+//               pollingInterval = null
+//             }
+//           } catch (e) {
+//             // Ignore DOMException: Blocked a frame with origin from accessing a cross-origin frame.
+//           }
+//         }, 250)
+//       })
+//     }
+//   },
+//   mounted () {
+//     this.iframeRef = document.getElementById('auth-iframe')
+//     this.url = this.getRefreshAuthURL
+//     this.pollIframe(this.getSilentRedirect, this.iframeRef)
+//       .then(({access_token, expires_in}) => {
+//         this.setAccessToken(access_token)
+//         this.setShowIframe(false)
+
+//         setTimeout(() => {
+//           this.setShowIframe(true)
+//           // Sets timeout value to 10 mins before expires_in
+//         }, (expires_in - 10 * 60) * 1000)
+//       })
+//       .catch(err => { 
+//         console.log(err)
+//       })
+//   }
+// }
 </script>
 
 <style scoped>
--- src/store/session.js
+++ src/store/session.js
@@ -1,57 +1,54 @@
 import { uuidv4 } from '../utils'
+import { defineStore } from 'pinia'
 
-const state = {
-  user: {},
-  userRoles: process.env.VUE_APP_TESTING === 'true' ? ['MCITY'] : null,
-  accessToken: process.env.VUE_APP_TESTING === 'true' ? 'test' : null,
-  oAuthServer: process.env.VUE_APP_OAUTH_SERVER,
-  oAuthKey: process.env.VUE_APP_OAUTH_KEY,
-  redirectURI: process.env.VUE_APP_REDIRECT_URI,
-  silentRedirectURI: `${process.env.VUE_APP_HOST}/refresh.html`,
-  oAuthScope: process.env.VUE_APP_OAUTH_SCOPE,
-  oAuthState: uuidv4(),
-  iframeRef: null,
-  showIframe: false
-}
-
-export const mutations = {
-  setUser (state, payload) {
-    state.user = payload
+export const useAuthStore = defineStore('authStore', {
+  state: () => {
+    return {
+      user: {},
+      userRoles: process.env.VUE_APP_TESTING === 'true' ? ['MCITY'] : null,
+      accessToken: process.env.VUE_APP_TESTING === 'true' ? 'test' : null,
+      oAuthServer: process.env.VUE_APP_OAUTH_SERVER,
+      oAuthKey: process.env.VUE_APP_OAUTH_KEY,
+      redirectURI: process.env.VUE_APP_REDIRECT_URI,
+      silentRedirectURI: `${process.env.VUE_APP_HOST}/refresh.html`,
+      oAuthScope: process.env.VUE_APP_OAUTH_SCOPE,
+      oAuthState: uuidv4(),
+      iframeRef: null,
+      showIframe: false
+    }
   },
-  setUserRoles (state, payload) {
-    state.userRoles = payload
+  actions: {
+    setUser (state, payload) {
+      state.user = payload
+    },
+    setUserRoles (state, payload) {
+      state.userRoles = payload
+    },
+    setAccessToken (state, payload) {
+      state.accessToken = payload
+    },
+    setIframeRef (state, payload) {
+      state.iframeRef = payload
+    },
+    setShowIframe (state, payload) {
+      state.showIframe = payload
+    }
   },
-  setAccessToken (state, payload) {
-    state.accessToken = payload
-  },
-  setIframeRef (state, payload) {
-    state.iframeRef = payload
-  },
-  setShowIframe (state, payload) {
-    state.showIframe = payload
+  getters: {
+    getUser: state => state.user,
+    getFullName: state => `${state.user.first_name} ${state.user.last_name}`,
+    getUserRoles: state => state.userRoles,
+    getAccessToken: state => state.accessToken,
+    getAuthURL: ({ oAuthServer, oAuthKey, redirectURI, oAuthScope, oAuthState } = state) => {
+      return `${oAuthServer}oauth/authorize?response_type=token&client_id=${oAuthKey}&redirect_uri=${redirectURI}&scope=${oAuthScope}&state=${oAuthState}`
+    },
+    getOAuthState: state => state.oAuthState,
+    getOAuthServer: state => state.oAuthServer,
+    getRefreshAuthURL: ({ oAuthServer, oAuthKey, silentRedirectURI, oAuthScope, oAuthState } = state) => {
+      return `${oAuthServer}oauth/authorize?response_type=token&client_id=${oAuthKey}&redirect_uri=${silentRedirectURI}&scope=${oAuthScope}&state=${oAuthState}`
+    },
+    getIframeRef: state => state.iframeRef,
+    getShowIframe: state => state.showIframe,
+    getSilentRedirect: state => state.silentRedirectURI
   }
-}
-
-const getters = {
-  getUser: state => state.user,
-  getFullName: state => `${state.user.first_name} ${state.user.last_name}`,
-  getUserRoles: state => state.userRoles,
-  getAccessToken: state => state.accessToken,
-  getAuthURL: ({ oAuthServer, oAuthKey, redirectURI, oAuthScope, oAuthState } = state) => {
-    return `${oAuthServer}oauth/authorize?response_type=token&client_id=${oAuthKey}&redirect_uri=${redirectURI}&scope=${oAuthScope}&state=${oAuthState}`
-  },
-  getOAuthState: state => state.oAuthState,
-  getOAuthServer: state => state.oAuthServer,
-  getRefreshAuthURL: ({ oAuthServer, oAuthKey, silentRedirectURI, oAuthScope, oAuthState } = state) => {
-    return `${oAuthServer}oauth/authorize?response_type=token&client_id=${oAuthKey}&redirect_uri=${silentRedirectURI}&scope=${oAuthScope}&state=${oAuthState}`
-  },
-  getIframeRef: state => state.iframeRef,
-  getShowIframe: state => state.showIframe,
-  getSilentRedirect: state => state.silentRedirectURI
-}
-
-export default {
-  state,
-  getters,
-  mutations
-}
+})
