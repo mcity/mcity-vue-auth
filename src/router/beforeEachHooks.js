536f4667bec3b960c2ab025fad1d6e51ac37107c
Ryan Lewis <accounts+github@ryandlewis.dev>
2023-08-10 11:30:02 -0500

    v1.4.5; combine checkRequiresAuth and checkRequiresApproved

--- src/router/beforeEachHooks.js
+++ src/router/beforeEachHooks.js
@@ -1,24 +1,23 @@
 export function checkRequiresAuth (to, from, next) {
   if (to.matched.some(route => route.meta.requiresAuth)) {
+    console.log('checking if user is authenticated')
     if (this.$store.getters.getAccessToken) {
-      next()
+      if (to.matched.some(route => route.meta.requiresApproved)) {
+        console.log('checking if user is approved')
+        console.log(this.$store.getters.getUserRoles)
+        if (this.$store.getters.getUserRoles.includes('UNAPPROVED') || this.$store.getters.getUserRoles.includes('BANNED')) {
+          console.log('user is not approved')
+          location.href = this.$store.getters.getUnapprovedURL
+        } else {
+          console.log('user is approved')
+          next()
+        }
+      }
     } else {
       localStorage.setItem(this.$store.getters.getOAuthState, to.fullPath)
       location.href = this.$store.getters.getAuthURL
     }
   } else {
     next()
-  }
-}
-
-export function checkRequiresApproved(to, from, next) {
-  if (to.matched.some(route => route.meta.requiresApproved)) {
-    console.log('checking if user is approved')
-    console.log(this.$store.getters.getUserRoles)
-    if (this.$store.getters.getUserRoles.includes('UNAPPROVED') || this.$store.getters.getUserRoles.includes('BANNED')) {
-      location.href = this.$store.getters.getUnapprovedURL
-    } else {
-      next()
-    }
   }
 }
