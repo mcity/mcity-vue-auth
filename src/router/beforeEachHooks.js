904af772e84ce9eb14cd6c8e9ba0662cd22cb7d9
Ryan Lewis <accounts+github@ryandlewis.dev>
2023-08-10 14:50:16 -0500

    Cleanup debugging

--- src/router/beforeEachHooks.js
+++ src/router/beforeEachHooks.js
@@ -1,15 +1,10 @@
 export function checkRequiresAuth (to, from, next) {
   if (to.matched.some(route => route.meta.requiresAuth)) {
-    console.log('checking if user is authenticated')
     if (this.$store.getters.getAccessToken) {
       if (to.matched.some(route => route.meta.requiresApproved)) {
-        console.log('checking if user is approved')
-        console.log(this.$store.getters.getUserRoles)
         if (this.$store.getters.getUserRoles.includes('UNAPPROVED') || this.$store.getters.getUserRoles.includes('BANNED')) {
-          console.log('user is not approved')
           location.href = this.$store.getters.getUnapprovedURL
         } else {
-          console.log('user is approved')
           next()
         }
       }
