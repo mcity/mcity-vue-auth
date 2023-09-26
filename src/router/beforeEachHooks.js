2ce392080e6e4bfebd0b23aa9c1813907d98f6ca
Darian Hogue <dhogue@m-c9gf7m32tp.local>
2023-09-26 12:07:25 -0400

    updated the beforeEachHook w/ pinia syntax

--- src/router/beforeEachHooks.js
+++ src/router/beforeEachHooks.js
@@ -1,14 +1,22 @@
-import { useUserStore } from "src/stores/userStore";
+import { useSessionStore } from "../store/session";
 
 export function checkRequiresAuth(to, from, next) {
-  const userStore = useUserStore();
-
-  if (to.meta.requiresAuth && to.meta.authorized === undefined) {
-    if (userStore.session.accessToken) {
-      next();
+  const sessionStore = useSessionStore();
+  if (to.matched.some((route) => route.meta.requiresAuth)) {
+    if (sessionStore.accessToken) {
+      if (to.matched.some((route) => route.meta.requiresApproved)) {
+        if (
+          sessionStore.getUserRoles.includes("UNAPPROVED") ||
+          sessionStore.getUserRoles.includes("BANNED")
+        ) {
+          location.href = sessionStore.unapprovedURL;
+        } else {
+          next();
+        }
+      }
     } else {
-      localStorage.setItem(userStore.session.oAuthState, to.fullPath);
-      location.href = userStore.getAuthURL;
+      localStorage.setItem(sessionStore.oAuthState, to.fullPath);
+      location.href = sessionStore.getAuthURL;
     }
   } else {
     next();
